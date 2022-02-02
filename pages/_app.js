82a7fdce7af6d3b4c8bb41bdad2cffb4118b0af8
Nick Ocampo <nick@politech.io>
2022-02-01 22:21:28 -0800

    set scroll to top behavior

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useRouter } from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, Router }) {
+  const router = useRouter();
+
+  router.events.on("routeChangeComplete", (url) => {
+    window.scroll({
+      top: 0,
+      left: 0,
+    });
+  });
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
